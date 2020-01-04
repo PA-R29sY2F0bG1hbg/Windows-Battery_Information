a1a6cf58feec1a73c1f415f855f4f754a67c00e7
Philippe-Alexandre <philippealexandremunch@gmail.com>
2020-01-04 18:37:40 +0100

    Add files via upload

---
+++ DATA/Battery_Information.py
@@ -0,0 +1,13 @@
+import psutil
+import tkinter as tk
+import tkMessageBox
+
+battery = psutil.sensors_battery()
+plugged = battery.power_plugged
+percent = battery.percent
+print percent
+if percent == 41:
+        # message box
+        root = tk.Tk()
+        root.withdraw()
+        tkMessageBox.showwarning("Battery Information", "Battery Fully Charged Please Unplug the charger")
